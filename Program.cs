46899b28ca3847a286aba9f1022bbff227d2f861
Phil Griswold <philgriswold@me.com>
2019-11-21 15:11:33 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace linq
 
             List<string> LFruits = fruits.Where(fruit =>
             {
-                bool startsWithL = fruit.StartsWith("L");
+                bool LFruit = fruit.StartsWith("L");
                 return startsWithL
             }).ToList();
 
@@ -48,15 +48,113 @@ namespace linq
 
             // Order these student names alphabetically, in descending order (Z to A)
             List<string> names = new List<string>()
-{
-    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
-    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
-    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
-    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
-    "Francisco", "Tre"
-};
+            {
+            "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+            "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+            "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+            "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+            "Francisco", "Tre"
+            };
 
-            List<string> descend = ...
+            List<string> descend = names;
+            descend.Sort();
+            descend.Reverse();
+
+            Console.WriteLine();
+            foreach (var name in descend)
+            {
+                Console.WriteLine(name);
+            }
+
+            List<string> anotherDescend = names.OrderByDescending(name => name).ToList();
+            Console.WriteLine();
+            foreach (var item in collection)
+            {
+                Console.WriteLine(name);
+            }
+
+            List<int> moreNumbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> orderedNumbers = moreNumbers.OrderBy(num => num).ToList();
+            Console.WriteLine();
+            foreach (var num in orderedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            moreNumbers.Sort();
+            Console.WriteLine();
+            foreach (var num in moreNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Output how many numbers are in the moreNumbers list
+            Console.WriteLine($"There are {moreNumbers.Count()} numbers in the moreNumbers List");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+
+            Console.WriteLine($"I spent {purchases.Sum()}. Too much!!");
+        }
+        List<double> prices = new List<double>()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+        double maxPrice = prices.Max();
+        Console.WriteLine($"OUR most expensive is worth {prices.Max()}");
+
+             List<int> wheresSquaredo = new List<int>()
+            {
+                66,
+                12,
+                8,
+                27,
+                82,
+                34,
+                7,
+                50,
+                19,
+                46,
+                81,
+                23,
+                30,
+                4,
+                68,
+                14
+            };
+        List<int> NOTPerfectSquares = wheresSquaredo.TakeWhile(num =>
+        {
+            bool isSquared = Math.Sqrt(num) % 1 == 0;
+            return !isSquared;
+        }).ToList();
+
+            foreach (var num in NOTPerfectSquares)
+            {
+                Console.WriteLine(num);
+            }
 
 
 
@@ -74,12 +172,7 @@ namespace linq
 
 
 
-
-
-
-
-
-            List<Customer> customers = new List<Customer>()
+    List<Customer> customers = new List<Customer>()
                 {
                     new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
                     new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
@@ -93,8 +186,8 @@ namespace linq
                     new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
                 };
 
-            var groups = customers.Where(c => c.Balance >= 1_000_000)
-                .GroupBy(c => c.Bank);
+    var groups = customers.Where(c => c.Balance >= 1_000_000)
+        .GroupBy(c => c.Bank);
 
             foreach (var group in groups)
             {
